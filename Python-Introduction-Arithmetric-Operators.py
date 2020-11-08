5fe827049be6017d3110d07976506b382158f4ee
hotlynn2 <laizer421@gmail.com>
2020-11-08 21:47:33 +0000

    Create Python-Introduction-Arithmetric-Operators.py

---
+++ Python-Introduction-Arithmetric-Operators.py
@@ -0,0 +1,21 @@
+# Task
+# The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+1. The first line contains the sum of the two numbers.
+2. The second line contains the difference of the two numbers (first - second).
+3. The third line contains the product of the two numbers.
+
+a = random(range(1, 10**10))
+b = random(range(1, 10**10))
+
+addition = a + b
+print(addition)\n
+
+subtraction = a - b
+print(subtraction)\n
+
+multiplication = a * b
+print(multiplication)
+
+
+
+
