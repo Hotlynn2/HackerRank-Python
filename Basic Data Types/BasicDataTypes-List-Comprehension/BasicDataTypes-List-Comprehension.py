e5c673162c8a8a6e421b0c2b3aa42f0f29723f86
hotlynn2 <laizer421@gmail.com>
2021-01-17 23:29:17 +0000

    Create BasicDataTypes-List-Comprehension.py

---
+++ Basic Data Types/BasicDataTypes-List-Comprehension/BasicDataTypes-List-Comprehension.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    l = list()
+
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if (i+j+k != n):
+                    l.append([i,j,k]) 
+    print(l)
+
+    [ in l.append[i,j,k] if (i+j+k !=n) for i in range(x+1) for j in range(y+1) for k in    range(z+1) ]
+    print(l)
+
+
