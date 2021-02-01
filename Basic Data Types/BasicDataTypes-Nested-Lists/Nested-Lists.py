03b6960cba53014bc2420bb3733e1841a276b359
hotlynn2 <laizer421@gmail.com>
2021-02-01 22:29:03 +0000

    Update Nested-Lists.py

--- Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
+++ Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
@@ -4,8 +4,23 @@ l;koj
 j;ljk;kljlk;;;;lk;l;
 
 
+j;ljk;kljlk;;;;lk;l;
+
+90888888888888888888888
 l;koj
 'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+j;ljk;kljlk;;;;lk;l;
+
+
+l;koj
+'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+j;ljk;kljlk;;;;lk;l;
+
+90888888888888888888888
+l;koj
+'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+j;ljk;kljlk;;;;lk;l;['
+
 j;ljk;kljlk;;;;lk;l;
 
 90888888888888888888888
