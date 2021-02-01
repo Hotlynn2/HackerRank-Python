cb995ae2ffab277c07fc349ceef1b13d2241f8fd
hotlynn2 <laizer421@gmail.com>
2021-02-01 23:54:19 +0000

    commit on 'master'

--- Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
+++ Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
@@ -13,28 +13,12 @@ j;ljk;kljlk;;;;lk;l;
 ;
 
 
+]aDSASDSADSADSADDWAREWREWQ
+RWQRWQEWQEQWEWQ'
 
+l;kojAkd>MAS.DM.,SMADLSA44
 
-j;ljk;kljlk;;;;lk;l;
-
-90888888888888888888888
-l;koj
-'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
-j;ljk;kljlk;;;;lk;l;
-
-
-l;koj
-'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
-j;ljk;kljlk;;;;lk;l;
-
-90888888888888888888888
-l;koj;
-
-adasd
-'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
-j;ljk;kljlk;;;;lk;l;['
-
-l;koj
+SDSADSADSADSA
 'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
 j;ljk;kljlk;;;;lk;l;
 
