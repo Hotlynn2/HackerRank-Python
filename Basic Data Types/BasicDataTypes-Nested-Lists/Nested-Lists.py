de2f92aeae00b72c4799e5baca04c99569fd293c
hotlynn2 <laizer421@gmail.com>
2021-02-03 23:25:35 +0000

    commit on 'master'

--- Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
+++ Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
@@ -1,14 +1,41 @@
-90888888888888888888888
-l;koj
-'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
-j;ljk;kljlk;;;;lk;l;
 
-
-j;ljk;kljlk;;;;lk;l;
 
 90888888888888888888888
 l;koj
 'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+
+;ljk;kljlk;;;;lk;l;
+
+90888888888888888888888
+l;koj
+'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+j;ljk;kljlk;;;;lk;l;
+;
+;ljk;kljlk;;;;lk;l;
+
+90888888888888888888888
+l;koj
+'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+j;ljk;kljlk;;;;lk;l;
+;
+
+j;ljk;kljlk;;;;lk;l;
+;
+;ljk;kl;ljk;kljlk;;;;lk;l;
+
+90888888888888888888888
+l;koj
+'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+j;ljk;kljlk;;;;lk;l;
+;
+jlk;;;;lk;l;
+
+90888888888888888888888
+l;koj
+'pk;lkkkkkkkk''''''''''''''''''''''''''''''''''
+j;ljk;kljlk;;;;lk;l;
+;
+'''''''''''''''''''
 j;ljk;kljlk;;;;lk;l;
 ;
 
