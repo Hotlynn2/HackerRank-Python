e4878d3273a02fb58e5c369fbf02ab32318cbd89
hotlynn2 <laizer421@gmail.com>
2021-02-10 17:01:20 +0000

    score

--- Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
+++ Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
@@ -4,4 +4,6 @@ scoresheet = []
 if __name__ == '__main__':
     for _ in range(int(input())):
         name = input()
-        score = float(input())
+        score = float(input()) 
+        marksheet = marksheet + [[name, score]]
+        score = score + [score] 
