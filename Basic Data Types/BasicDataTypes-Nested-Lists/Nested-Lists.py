87d2278a6b5a9f5efb7a24062ed1b34bfb1843a4
hotlynn2 <laizer421@gmail.com>
2021-02-10 17:25:28 +0000

    commit

--- Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
+++ Basic Data Types/BasicDataTypes-Nested-Lists/Nested-Lists.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     for _ in range(int(input())):
         name = input()
         score = float(input()) 
-        marksheet = marksheet + [[name, score]]
-        score = score + [score] 
-        x = sorted(set(scoresheet))[1]
+        marksheet+=[[name, score]]
+        scoresheet+=[score] 
+    x = sorted(set(scoresheet))[1]
 
-    for n,s in sorted(markssheet)[1]:
+    for n,s in sorted(marksheet):
         if s==x:
             print(n)
