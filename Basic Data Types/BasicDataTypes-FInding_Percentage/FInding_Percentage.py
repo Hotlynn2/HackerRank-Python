c89e7f513f740219af38ddbb4e199868dec13e40
hotlynn2 <laizer421@gmail.com>
2021-02-14 23:17:42 +0000

    commit

--- Basic Data Types/BasicDataTypes-FInding_Percentage/FInding_Percentage.py
+++ Basic Data Types/BasicDataTypes-FInding_Percentage/FInding_Percentage.py
@@ -5,9 +5,13 @@ if __name__ == '__main__':
     
     for _ in range(n):
         score_data = str(input()).split()
+        student_name.append(score_data[0])
+        student_score.append(score_data[1:])
+    print(student_name)
+    print(student_score) 
         
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
+        #name, *line = input().split()
+        #scores = list(map(float, line))
+        #student_marks[name] = scores
 
-    query_name = input()
+    #query_name = input()
