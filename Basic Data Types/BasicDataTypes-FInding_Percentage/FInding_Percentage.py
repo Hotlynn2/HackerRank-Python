d7fbb321b614fd1c67e686c6fdf5a831a8373d3e
hotlynn2 <laizer421@gmail.com>
2021-03-05 23:41:00 +0000

    commit

--- Basic Data Types/BasicDataTypes-FInding_Percentage/FInding_Percentage.py
+++ Basic Data Types/BasicDataTypes-FInding_Percentage/FInding_Percentage.py
@@ -9,17 +9,22 @@ if __name__ == '__main__':
     for _ in range(n):
         score_data = str(input()).split()
         student_name.append(score_data[0])
-        student_score.append(score_data[1:])
+        student_score.append(int(score_data[1:]))
 
     query_name = input()
+    
+    '''score_int = []
+    for score in student_score:
+        score_int.appendint()'''
 
     students = dict(zip(student_name, student_score))  
     print(students)     
 
     #student_names = student_name.split()
     for name,score in students.items():
-            if (name == query_name):
-                return sum(vslur) / 3
+            if (name == query_name): 
+                average = sum(score) / 3
+    print(average)           
 
         
 
